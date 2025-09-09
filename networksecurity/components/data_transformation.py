8e519e9b2177cde0fbd0bd65fced3fc1992a684e
Atharva Rai <atharvarai07@gmail.com>
2025-09-09 14:23:28 +0530

    Add saving functionality for preprocessor and trained model in data transformation and model trainer

--- networksecurity/components/data_transformation.py
+++ networksecurity/components/data_transformation.py
@@ -67,6 +67,8 @@ class DataTransformation:
             save_numpy_array_data(self.data_transformation_config.transformed_test_file_path, array=test_arr)
             save_object(self.data_transformation_config.transformed_object_file_path, obj=preprocessor_obj)
 
+            save_object("final_model/preprocessor.pkl",obj=preprocessor_obj)
+
             data_transformaton_artifact = DataTransformationArtifact(
                 transformed_object_file_path=self.data_transformation_config.transformed_object_file_path,
                 transformed_train_file_path=self.data_transformation_config.transformed_train_file_path,
--- networksecurity/components/model_trainer.py
+++ networksecurity/components/model_trainer.py
@@ -101,6 +101,8 @@ class ModelTrainer:
             Network_Model = NetworkModel(preprocessor=preprocessor, model=best_model)
             save_object(file_path=self.model_trainer_config.trained_model_file_path, obj=Network_Model)
 
+            save_object("final_model/model.pkl",obj=best_model)
+
             self.track_mlflow(best_model=best_model,classification_train_metric=classification_train_metric,classification_test_metric=classification_test_metric)
 
             model_trainer_artifact = ModelTrainerArtifact(
